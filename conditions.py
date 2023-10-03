1d50a878c12dc7d89fa30f4cf878adc137d59ca3
Collingvn <146225186+Collingvn@users.noreply.github.com>
2023-10-03 16:12:45 -0400

    Update conditions.py

--- conditions.py
+++ conditions.py
@@ -1,1 +1,18 @@
+# CS104
+# First name Last name
+# conditions.py
 
+# Prompting the user for input
+temp = int(input("Please enter the current temperature: "))
+
+# Checking temperature conditions and giving recommendations
+if temp > 90:
+    print("Wear Shorts")
+elif temp > 70:
+    print("Short sleeves are fine")
+elif temp > 50:
+    print("Wear a jacket")
+elif temp > 32:
+    print("Wear a heavy coat")
+else:
+    print("Stay Inside")
